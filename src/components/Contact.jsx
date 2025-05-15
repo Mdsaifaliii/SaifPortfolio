b8963fbf8163b2df230f937fdb861c44f461da1b
Mdsaifaliii <your-saifali79873@gmail.com>
2025-05-15 15:47:31 +0530

    https://github.com/Mdsaifaliii/SaifPortfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { firestoreDB } from "../firebase"; // Import Firestore instance
+import { firestoreDB } from "./Firebase"; // Import Firestore instance
 import { collection, addDoc } from "firebase/firestore"; // Import Firestore 
-import app from '../firebase';
+import app from './Firebase';
 
 
 function Contact() {
