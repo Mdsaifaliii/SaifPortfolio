03ed2f11b2b3dcd4dfedcba304c3d73411e23db8
Mdsaifaliii <saifali79873@gmail.com>
2026-02-03 19:12:51 +0530

    https://github.com/Mdsaifaliii/SaifPortfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,50 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa"; // Importing social media icons
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa"; // Importing social media icons
+import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2025 Mohd Saif Ali. All Rights Reserved.</p>
-        
+
         {/* Social Media Icons */}
-        <div className="mt-2">
+        <div className="mt-3 flex justify-center items-center gap-6 text-blue-400">
           <a
             href="https://www.linkedin.com/in/mohd-saif-ali-software-developer/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-400 mr-4 hover:text-blue-600 transition duration-300"
+            className="hover:text-blue-600 transition duration-300"
           >
-            <FaLinkedin className="w-6 h-6 inline" />
+            <FaLinkedin size={22} />
           </a>
-          {/* <a
-            href="https://github.com/Mdsaifaliii"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-400 mr-4 hover:text-blue-600 transition duration-300"
-          >
-            <FaGithub className="w-6 h-6 inline" />
-          </a> */}
+          {/* 
+  <a
+    href="https://github.com/Mdsaifaliii"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="hover:text-blue-600 transition duration-300"
+  >
+    <FaGithub size={22} />
+  </a> 
+  */}
+
           <a
-            href="https://twitter.com"
+            href="https://x.com/yourusername"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-400 mr-4 hover:text-blue-600 transition duration-300"
+            className="hover:text-blue-600 transition duration-300"
           >
-            <FaTwitter className="w-6 h-6 inline" />
+            <FaXTwitter size={22} />
           </a>
+
           <a
-            href="https://www.instagram.com/md_saifaly/?igsh=ZGZoN3gyMjA5c3hr#"
+            href="https://www.instagram.com/md_saifaly/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-400 hover:text-blue-600 transition duration-300"
+            className="hover:text-blue-600 transition duration-300"
           >
-            <FaInstagram className="w-6 h-6 inline" />
+            <FaInstagram size={22} />
           </a>
         </div>
       </div>
